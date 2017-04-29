6b78481edb9b9a4006da1b000f7de6b9469ed6ac
Arun Bharathi <arun.bharathi.kumarasamy@sap.com>
2017-04-29 15:39:20 +0530

    added simulation data logic for populaition

--- scripts/models/gameAlgorithms.js
+++ scripts/models/gameAlgorithms.js
@@ -24,9 +24,12 @@ function exp_smoothing(inputArray, alpha){
 		function(gameService) {
 			
 			var currentData = '';
+			
 			function GameAlgorithms() {
 				//adding the entire game data
 				this.gameData = "";
+				this.gameBeginYear = 2000;
+				this.gameEndYear = 2016;
 				//newly updated data or temp data
 				this._currentGameData = "";
 			};
@@ -42,32 +45,35 @@ function exp_smoothing(inputArray, alpha){
 							var _data = data.data.NASADATA.cities[0];
 							//self._gameData.Population_2010_16 = _data.Population_2010_16[0];
 							//self._gameData.Temprature = _data.Temprature[0];
-							self.loadCity(data.data.NASADATA.cities[0]);
+							self.loadCity(data.data.NASADATA.cities[0],self.gameBeginYear,self.gameEndYear);
+							//var oRunSimulation = setTimeout(loadCity(gameData),);
 							return self._gameData;
 							console.log('Fetching city data complete');
 						})
 					}
 					
 					//To be called on a time series basis
-					function loadCity(gameData){
+					function loadCity(gameData , i ,gameEndYear){
+							if (i==gameEndYear+1){
+								return;
+							}					
 							console.log("City simulation in progress");
-							var i = 2000;
+							//var i = 	;
 							var _population = [];
 							var _numberOfInitBuildings;
-							for (i;i<2017;i++) {
+							/*for (this.gameBeginYear;i<2017;i++) {
 								_population.push(gameData.Population_2010_16[0][i]);
 								console.log(gameData.Population_2010_16[0][i]);
-							}
+							}*/
 							//start with these number of buildings
-							_numberOfInitBuildings = gameData.Population_2010_16[0][2000]/(gameData.Population_2010_16[0][2016]/40);
-							_numberOfVechicles = gameData.Population_2010_16[0][2000]/(gameData.Population_2010_16[0][2016]/30);
+							_numberOfInitBuildings = Math.trunc(gameData.Population_2010_16[0][i]/(gameData.Population_2010_16[0][gameEndYear]/40));
+							_numberOfVechicles = Math.trunc(gameData.Population_2010_16[0][i]/(gameData.Population_2010_16[0][gameEndYear]/30));
 							
 							//number of tress - the current temprature
-							_numberOfTrees = 60 - gameData.Temprature[0][2016].split(',')[0];
-							console.log(_population);
-							console.log(_numberOfVechicles);
-							console.log(_numberOfTrees);							
-							console.log(_numberOfInitBuildings);
+							_numberOfTrees = Math.trunc(60 - gameData.Temprature[0][i].split(',')[0]);
+							console.log(_numberOfVechicles+ ' and  '+_numberOfTrees+' and '+_numberOfInitBuildings);
+							//var oRunSimulation = setTimeout(loadCity(gameData,i+1),300);
+							setTimeout(loadCity(gameData,i+1,gameEndYear),100);
 					}
 					
 					function exp_smoothing(inputArray, alpha){
