b1461284ce5804b586b40cfb0cf2a4199c90c9d5
Arun Bharathi <arun.bharathi.kumarasamy@sap.com>
2017-04-29 15:02:15 +0530

    added load city function

--- scripts/models/gameAlgorithms.js
+++ scripts/models/gameAlgorithms.js
@@ -42,18 +42,32 @@ function exp_smoothing(inputArray, alpha){
 							var _data = data.data.NASADATA.cities[0];
 							//self._gameData.Population_2010_16 = _data.Population_2010_16[0];
 							//self._gameData.Temprature = _data.Temprature[0];
-							self.updateCity(data.data.NASADATA.cities[0]);
+							self.loadCity(data.data.NASADATA.cities[0]);
 							return self._gameData;
 							console.log('Fetching city data complete');
 						})
 					}
 					
 					//To be called on a time series basis
-					function updateCity(gameData){
+					function loadCity(gameData){
 							console.log("City simulation in progress");
-							for(i=2000;i<2017;i++) {
+							var i = 2000;
+							var _population = [];
+							var _numberOfInitBuildings;
+							for (i;i<2017;i++) {
+								_population.push(gameData.Population_2010_16[0][i]);
 								console.log(gameData.Population_2010_16[0][i]);
 							}
+							//start with these number of buildings
+							_numberOfInitBuildings = gameData.Population_2010_16[0][2000]/(gameData.Population_2010_16[0][2016]/40);
+							_numberOfVechicles = gameData.Population_2010_16[0][2000]/(gameData.Population_2010_16[0][2016]/30);
+							
+							//number of tress - the current temprature
+							_numberOfTrees = 60 - gameData.Temprature[0][2016].split(',')[0];
+							console.log(_population);
+							console.log(_numberOfVechicles);
+							console.log(_numberOfTrees);							
+							console.log(_numberOfInitBuildings);
 					}
 					
 					function exp_smoothing(inputArray, alpha){
@@ -70,7 +84,7 @@ function exp_smoothing(inputArray, alpha){
 					return {
 						getCityData : getCityData,
 						exp_smoothing : exp_smoothing,
-						updateCity : updateCity
+						loadCity : loadCity
 					}
 				})();			
 		 	return {
